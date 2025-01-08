65028bcedcc2f411487bd90f7c82e44946d86641
BolNick <BolNick09RUS@gmail.com>
2025-01-08 18:19:54 +0300

    Добавьте файлы проекта.

---
+++ UdpShapes.Client/PlayerVM.cs
@@ -0,0 +1,9 @@
+﻿namespace UdpShapes.Client;
+
+public class PlayerVM {
+    // Без уведомления об изменении, потому что данные меняет только пользователь своими действиями.
+    // Если программа не меняет данные, то можно без INotifyPropertyChanged.
+    public string Name { get; set; }
+
+
+}
